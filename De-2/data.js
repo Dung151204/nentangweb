f87b849d174a49370cd0e2b8663729c079c0014f
Dung151204 <dungyoshono2004@gmail.com>
2025-08-02 14:43:07 +0700

    De 123 them jquerry

---
+++ De-2/data.js
@@ -0,0 +1,7 @@
+const data = [
+  { id: 1102, customer: "Võ Hoài An", staff: "Mai Thục Anh", amount: "250000", date: "06 Tháng 6 2024 9:00" },
+  { id: 1199, customer: "Hoàng Thị Thắng", staff: "Nguyễn Văn Hồng", amount: "600000", date: "06 Tháng 6 2024 9:03" },
+  { id: 1239, customer: "Nguyễn Huy Quang", staff: "Nguyễn Văn Hồng", amount: "934000", date: "06 Tháng 6 2024 9:10" },
+  { id: 1677, customer: "Huỳnh Văn Nam", staff: "Mai Thục Anh", amount: "150000", date: "06 Tháng 6 2024 9:20" },
+  { id: 1439, customer: "Nguyễn Hồng Minh", staff: "Mai Thục Anh", amount: "354000", date: "06 Tháng 6 2024 9:24" }
+];
---
+++ De-3/data.js
@@ -0,0 +1,30 @@
+const nhanVienData = [
+  {
+    stt: 1,
+    ten: "Mai",
+    hodem: "Thục Anh",
+    diachi: "23 Hoàng Đạo Thúy, Thanh Xuân, Hà Nội",
+    hoatdong: false
+  },
+  {
+    stt: 2,
+    ten: "Hoàng",
+    hodem: "Sinh Hồng",
+    diachi: "23 Hoàng Đạo Thúy, Thanh Xuân, Hà Nội",
+    hoatdong: true
+  },
+  {
+    stt: 3,
+    ten: "Tính",
+    hodem: "Mai Trung",
+    diachi: "1411 Đường Láng, Cầu Giấy, Hà Nội",
+    hoatdong: true
+  },
+  {
+    stt: 4,
+    ten: "Hồng",
+    hodem: "Nguyễn Văn",
+    diachi: "1411 Đường Láng, Cầu Giấy, Hà Nội",
+    hoatdong: false
+  }
+];
